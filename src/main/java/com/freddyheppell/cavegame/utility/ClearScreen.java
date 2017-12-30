9b2f5fcaecb0946f2d9d6e4e4957e9aa74f46c73
Freddy Heppell <f.heppell@gmail.com>
2017-12-30 18:10:27 +0000

    Added *nix clear screen implementation

--- src/main/java/com/freddyheppell/cavegame/utility/ClearScreen.java
+++ src/main/java/com/freddyheppell/cavegame/utility/ClearScreen.java
@@ -5,14 +5,36 @@ import com.freddyheppell.cavegame.config.Config;
 import java.io.IOException;
 
 public class ClearScreen {
+
+    /**
+     * Is the computer a Windows computer?
+     */
+    private static final boolean isWindows = System.getProperty("os.name").startsWith("Windows");
+
+    /**
+     * String to clear screen on *nix systems (mac OS/Linux etc.)
+     * First character clears screen, second returns cursor to beginning of screen
+     */
+    private static final String unixClearString = "\033[H\033[2J";
+
     public static void clear() {
-        if (Config.SHOULD_CLEAR_SCREEN) {
+        if (!Config.SHOULD_CLEAR_SCREEN) {
+            // If screen clearing is disabled in the configuration,
+            // don't clear it
+            return;
+        }
+
+        if (isWindows) {
+            // On windows, run `cls` in the command prompt
             try {
                 new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
             } catch (InterruptedException | IOException e) {
                 e.printStackTrace();
                 throw new RuntimeException("Error clearing screen");
             }
+        } else {
+            // On *nix systems, use the escape characters
+            System.out.print(unixClearString);
         }
     }
 }
